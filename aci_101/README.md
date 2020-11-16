ce035a7433593ef5cb7f94d5e4f076eb4d788032
Nik Sheridan <nik.sheridan@gmail.com>
2020-11-16 17:03:51 +0000

    adding more context to a typical use case

---
+++ aci_101/README.md
@@ -0,0 +1,3 @@
+# ACI 101 Workshop
+
+This is some text
--- aci_101/main.tf
+++ aci_101/main.tf
@@ -7,10 +7,85 @@ provider "aci" {
 }
 
 /*
-	Some generic comment
+	This section relates to creation of a tenant
 */
-resource "aci_tenant" "tenant" {
+resource "aci_tenant" "workshop1_tnt" {
 	name = "workshop1"
 	description = "workshop1"
 }
 
+/*
+	This section relates to creation of a vrf
+*/
+
+resource "aci_vrf" "workshop1_vrf1" {
+	tenant_dn = aci_tenant.workshop1_tnt.id
+	name      = "workshop_vrf1"
+	annotation = "workshop"
+}
+
+/*
+	This section relates to creation of a bridge domains
+*/
+resource "aci_bridge_domain" "dynamic_services_bd1" {
+	tenant_dn          = aci_tenant.workshop1_tnt.id
+	relation_fv_rs_ctx = aci_vrf.workshop1_vrf1.id
+	name               = "dynamic_services_bd1"
+}
+
+resource "aci_bridge_domain" "fixed_services_bd1" {
+	tenant_dn          = aci_tenant.workshop1_tnt.id
+	relation_fv_rs_ctx = aci_vrf.workshop1_vrf1.id
+	name               = "fixed_services_bd1"
+}
+
+/*
+	This section relates to creation of a application profile and EPG
+*/
+
+resource "aci_application_profile" "kubernetes1" {
+	tenant_dn = aci_tenant.workshop1_tnt.id
+	name      = "kubernetes"
+}
+resource "aci_application_epg" "cluster1" {
+	application_profile_dn  = aci_application_profile.kubernetes1.id
+	relation_fv_rs_bd       = aci_bridge_domain.dynamic_services_bd1.id
+	name                    = "cluster1"
+	# consume a contract
+	#relation_fv_rs_cons    = [
+  #	aci_contract.web_services.id
+	#]
+}
+resource "aci_application_epg" "cluster2" {
+	application_profile_dn  = aci_application_profile.kubernetes1.id
+	relation_fv_rs_bd       = aci_bridge_domain.dynamic_services_bd1.id
+	name                    = "cluster2"
+	# consume a contract
+	#relation_fv_rs_cons    = [
+  #	aci_contract.web_services.id
+	#]
+}
+
+
+resource "aci_application_profile" "fixed_services_db" {
+	tenant_dn = aci_tenant.workshop1_tnt.id
+	name      = "databases"
+}
+resource "aci_application_epg" "database1" {
+	application_profile_dn  = aci_application_profile.fixed_services_db.id
+	relation_fv_rs_bd       = aci_bridge_domain.fixed_services_bd1.id 
+	name                    = "database1"
+	# consume a contract
+	#relation_fv_rs_cons    = [
+  #	aci_contract.web_services.id
+	#]
+}
+resource "aci_application_epg" "database2" {
+	application_profile_dn  = aci_application_profile.fixed_services_db.id
+	relation_fv_rs_bd       = aci_bridge_domain.fixed_services_bd1.id 
+	name                    = "database2"
+	# consume a contract
+	#relation_fv_rs_cons    = [
+  #	aci_contract.web_services.id
+	#]
+}
