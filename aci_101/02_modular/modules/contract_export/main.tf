92dd10e5a45a20ff7f523230e70ced672ea4a25d
Nik Sheridan <nik.sheridan@gmail.com>
2020-11-16 22:41:14 +0000

    parameterising json doc for contract export

--- aci_101/02_modular/modules/contract_export/main.tf
+++ aci_101/02_modular/modules/contract_export/main.tf
@@ -2,27 +2,26 @@
 resource "aci_rest" "exported_contact" {
   path = "/api/mo/${var.tenant_destination_id}/cif-exported_contact.json"
 	#depends_on = var.contract_to_export
-  payload = <<EOF
-{
-  "imdata": [
+  payload = jsonencode({
+  "imdata"=[
 		{
-		"vzCPIf": {
-			"attributes": {
-					"annotation": "",
-					"descr": "this contract is a banana",
-					"dn": "uni/tn-${var.tenant_source_name}/cif-exported_contact",
-					"name": "exported_contact",
-					"nameAlias": "",
-					"ownerKey": "",
-					"ownerTag": ""
+		"vzCPIf"={
+			"attributes"={
+					"annotation"="",
+					"descr"="this contract is a banana",
+					"dn"="uni/tn-${var.tenant_source_name}/cif-exported_contact",
+					"name"="exported_contact",
+					"nameAlias"="",
+					"ownerKey"="",
+					"ownerTag"=""
 				},
-				"children": [
+				"children"=[
 					{
-						"vzRsIf": {
-							"attributes": {
-								"annotation": "",
-								"prio": "unspecified",
-								"tDn": "uni/tn-${var.tenant_destination_name}/brc-${var.contract_to_export}"
+						"vzRsIf"={
+							"attributes"={
+								"annotation"="",
+								"prio"="unspecified",
+								"tDn"="uni/tn-${var.tenant_destination_name}/brc-${var.contract_to_export}"
 							}
 						}
 					}
@@ -30,6 +29,6 @@ resource "aci_rest" "exported_contact" {
 			}
 		}
   ]
+})
 }
-	EOF
-}
+
